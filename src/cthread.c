718bb89a603620155f20054faddb67a4fa3aa003
gabriel <gabriel.stepien@inf.ufrgs.br>
2019-04-11 23:38:03 -0300

    preparing the house

--- src/cthread.c
+++ src/cthread.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "ucontext.h"
+
+#include "../include/cthread.h"
+#include "../include/support.h"
+#include "../include/cdata.h"
+
+FILA2 ready, executing, blocked;
+ucontext_t *final_context = NULL;
+s_TCB *main_tcb = NULL;
+
 
 
 
@@ -7,6 +20,13 @@
 int ccreate (void* (*start)(void*), void *arg, int prio) {
 
     // save the existing context (for the first run is the main)
+    ucontext_t *previous_context = (ucontext_t*) malloc(sizeof(ucontext_t));
+
+
+    // alloc tcb
+    TCB_t *tcb = (TCB_t *) malloc(sizeof(TCB_t));
+
+
     //
 
 

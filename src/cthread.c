f1f33dee691e5a03706ec1a5eaa1f3bed0056495
gabriel <gabriel.stepien@inf.ufrgs.br>
2019-04-11 23:23:14 -0300

    cmake

---
+++ src/cthread.c
@@ -0,0 +1,13 @@
+
+
+
+//
+// Created by gabriel on 11/04/19.
+//
+int ccreate (void* (*start)(void*), void *arg, int prio) {
+
+    // save the existing context (for the first run is the main)
+    //
+
+
+}

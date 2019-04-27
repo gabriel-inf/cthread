3958e72f4e8ead1a7f506c533c6028e46237c5e4
Laura Corssac <lauracorssac@hotmail.com>
2019-04-27 19:40:42 -0300

    i think i did cwait

--- exemplos/exemplo.c
+++ exemplos/exemplo.c
@@ -10,10 +10,13 @@
 
 #include "../include/support.h"
 #include "../include/cthread.h"
+#include "../include/cdata.h"
 #include <stdio.h>
 #include <stdlib.h>
 #define SUCCESS_CODE 0
 #define ERROR_CODE 10
+
+PFILA2 executing;
 
 /*
 void* func0(void *arg) {
@@ -41,6 +44,26 @@ int inicia(csem_t *sem, int count) {
 	}
 }
 
+int espera(csem_t *sem) {
+
+	sem->count --;
+	if (sem->count < 0) {
+		
+		if ( FirstFila2(executing) != SUCCESS_CODE ) return ERROR_CODE;
+
+		TCB_t *executing_process = (TCB_t *)executing->first->node;
+		executing_process->state = PROCST_BLOQ;
+
+		if (DeleteAtIteratorFila2(executing) != SUCCESS_CODE) return ERROR_CODE;
+		printf("deu certo delecao do executando\n");
+		
+		if (AppendFila2(sem->fila, &executing_process) != SUCCESS_CODE) return ERROR_CODE;
+		printf("deu certo append do executando na fila do semaphoro\n");
+
+	}
+	return SUCCESS_CODE;
+}
+
 int main(int argc, char *argv[]) {
 
 	/*int	id0, id1;
@@ -57,13 +80,37 @@ int main(int argc, char *argv[]) {
 	//printf("Eu sou a main voltandpara terminar o\n");
 	printf("Como Assim\n");
 
+	
+	TCB_t *tcb_teste = malloc(sizeof(TCB_t));
+	tcb_teste->prio = 1;
+	tcb_teste->state = PROCST_EXEC;
+	tcb_teste->tid = 10;
+	ucontext_t test_context;
+	tcb_teste->context = test_context;
+
 	csem_t *semaphore = malloc(sizeof(csem_t));
 	printf("aloooooooọ\n");
 
-	int result = inicia(semaphore, 100);
-	printf("Resultado inicializacao do semaforo =\n");
-	printf("%d\n", result);	
-	printf("%d\n", semaphore->count);
+	if (inicia(semaphore, 0) == SUCCESS_CODE ) {
+
+		printf("deu certo inicializacao do semaphoro\n");
+
+	}
+	
+	executing = malloc(sizeof(FILA2));
+	if (CreateFila2(executing) == SUCCESS_CODE) {
+
+		printf("deu certo creation\n");
+
+		if ( AppendFila2(executing, (void *)tcb_teste) == SUCCESS_CODE ) {
+
+			printf("deu certo append\n");
+			if ( espera(semaphore) == SUCCESS_CODE ) {
+				printf("deu certo espera\n");
+
+			}
+		}  
+	}
 
 	return 0;
 

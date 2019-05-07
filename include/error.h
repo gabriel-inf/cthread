587a776c66616f9d1a6bb763a8759de2e0761a65
Rodrigo Cardoso Buske <rodbuske@gmail.com>
2019-05-07 00:53:06 -0300

    Fix error codes

--- include/error.h
+++ include/error.h
@@ -4,11 +4,10 @@
 #define FAILED -1
 #define EMPTY_LINE -2
 #define NULL_POINTER -3
-#define LINE_OPERATION_ERROR -3 // FIXME: this has the same value as NULL_POINTER
-#define CONTEXT_ERROR -4 // FIXME: This error is not being used
 #define ERROR_PRIO_NOT_DEFINED -5
 #define NOTHING_TO_SCHEDULE -6
 #define MALLOC_ERROR -7
+#define LINE_OPERATION_ERROR -8
 
 #define ERROR_CREATING_MAIN -11
 
